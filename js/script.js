8fcd6fa3f46d8ddc5fc8431a7763cd888497d84a
Richard Hpa <richard.hpa@acgedu.com>
2018-01-30 13:31:57 +1300

    added basic d3

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// d3.select('.node').text('Selected');
+
+// d3.selectAll('.node').text('Selected');
+
+// d3.select('.node:nth-child(3)').text('Selected');
+
+// d3.selectAll('.node:nth-child(even)').html('<strong>Selected</strong>');
+
+// d3.select('.node')
+//     .text('this is the parent node')
+//     .append('div')
+//         .html('<strong>This div was appended</strong>')
+//         .append('div')
+//             .html("<small>This is appeneded to the append</small>");
+
+// d3.select('#NodeContainer')
+//     .insert('span', ':nth-child(3)')
+//         .html('<strong>Inserted in the 3rd</strong>');
+
+// d3.select('#NodeContainer .node:nth-child(4)')
+//     .remove();
+
+// d3.selectAll('.node')
+//     .attr('class', 'newClass');
+
+// d3.selectAll('.node')
+//     .classed('newClass divs', true)
+//     .classed('node', false);
+
+// d3.selectAll('.node:nth-child(3)')
+//     .style("color", "red")
+//     .style("background-color", "blue")
+//     .style('text-decoration', 'underline');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("d3 project");
