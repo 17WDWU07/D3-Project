d13f785779cbb3bdf6f395dfebbb8e79b4d84b53
Richard Hpa <richard.hpa@acgedu.com>
2018-01-31 13:15:33 +1300

    added scales to the bar chart

--- js/barchart.js
+++ js/barchart.js
@@ -1,8 +1,16 @@
-var barData = [12,56,12,84,15,45,15,97,100,2,16,45];
+var barData = [0,12,56,12,84,15,45,15,97,100,2,16,45,120, 200, 50];
 var height = 400,
     width = 600,
     barWidth = 50,
     barOffset = 5;
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(barData)])
+    .range([0, height])
+
+var xScale = d3.scaleBand()
+    .domain(d3.range(0, barData.length))
+    .range([0, width])
 
 d3.select('body').append('svg')
     .attr('width', width)
@@ -13,15 +21,15 @@ d3.select('body').append('svg')
         .enter().append('rect')
             .classed('bar', true)
             .style('fill', '#c0392b')
-            .attr('width', barWidth)
+            .attr('width', xScale.bandwidth())
             .attr('height', function(d){
-                return d
+                return yScale(d)
             })
             .attr('x', function(d,i){
-                return i * (barWidth+barOffset)
+                return xScale(i)
             })
             .attr('y', function(d){
-                return height - d
+                return height - yScale(d)
             }).exit()
 
 
